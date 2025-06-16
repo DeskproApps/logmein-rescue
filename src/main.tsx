cc199189a765d22f92a59d40e9406ca0bcbc0666
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-06-16 17:39:23 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#40)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import './instrument';
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
 import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import { queryClient } from "./query";
 import { App } from "./App";
@@ -13,15 +13,18 @@ import "tippy.js/dist/tippy.css";
 import "simplebar/dist/simplebar.min.css";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render((
   <StrictMode>
     <DeskproAppProvider>
       <HashRouter>
         <QueryClientProvider client={queryClient}>
           <Suspense fallback={<LoadingSpinner/>}>
-            <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <ErrorBoundary fallback={ErrorFallback}>
               <App />
             </ErrorBoundary>
           </Suspense>
